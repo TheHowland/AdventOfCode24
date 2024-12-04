7afe289908b3f1fcb9375ecd438ab085468065f8
Yannick Wieland <yannick.wieland@gmail.com>
2024-12-04 16:09:05 +0100

    day2 part 2 working

--- Day2.cpp
+++ Day2.cpp
@@ -3,51 +3,86 @@
 #include <algorithm>
 #include <fstream>
 
-void read_file(std::vector<int>* list) {
-    int a,b,c,d,e;
-    int val;
-    while(std::cin >> a >> b >> c >> d >> e) {
-      val = 1;
-      if ((a-b) > 0 && (b-c) > 0 && (c-d) > 0 && (d-e) > 0) {
-          val = ((unsigned int)(a-b) >> 2) + ((unsigned int)(b-c) >> 2) + ((unsigned int)(c-d) >> 2) + ((unsigned int)(d-e) >> 2);
-      }
-      if ((b-a) > 0 && (c-b) > 0 && (d-c) > 0 && (e-d) > 0) {
-          val = ((unsigned int)(b-a) >> 2) + ((unsigned int)(c-b) >> 2) + ((unsigned int)(d-c) >> 2) + ((unsigned int)(e-d) >> 2);
-      }
-      list->push_back(val);
+bool isDecending(std::vector<unsigned int>& v) {
+    for (int i = 0; i < v.size() - 1; i++) {
+        if (v[i] <= v[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isAscending(std::vector<unsigned int>& v) {
+    for (int i = 0; i < v.size() - 1; i++) {
+        if (v[i] >= v[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+unsigned int checkDist(std::vector<unsigned int>& v) {
+    unsigned int val = 0;
+    for (int i = 0; i < v.size() - 1; i++) {
+        val += static_cast<bool>((v[i]-v[i+1]) >> 2);
+    }
+    return val;
+}
+
+unsigned int validateSequence(std::vector<unsigned int>& v) {
+    if (isAscending(v)) {
+        std::ranges::reverse(v);
+        return checkDist(v);
+    }
+    if (isDecending(v)) {
+        return checkDist(v);
+    }
+    return 1;
+}
+
+unsigned int checkSequenceOptions(std::vector<unsigned int>& v) {
+    for (int i = 0; i < v.size(); i++) {
+        std::vector<unsigned int> temp = v;
+        temp.erase(temp.begin() + i);
+
+        if (!validateSequence(temp)) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void read_file(std::vector<unsigned int>* list) {
+    std::vector<unsigned int> dataLine;
+    std::string line;
+    unsigned int a;
+    char buffer[25];
+
+    while (std::cin.getline(buffer, 25)) {
+        dataLine.clear();
+        std::istringstream iss(buffer);
+        while (iss >> a) {
+            dataLine.push_back(a);
+        }
+        unsigned int returnVal = validateSequence(dataLine);
+        if (returnVal) {
+            returnVal = checkSequenceOptions(dataLine);
+        }
+        list->push_back(returnVal);
     }
 }
 
 int main(void) {
-    std::vector<int> list;
+    std::vector<unsigned int> list;
     read_file(&list);
 
-    unsigned int all = list.size();
-    int unsafe = 0;
-    for (int i = 0; i < list.size(); i++) {
-      if (list[i] != 0){
-          all--;
-      }
+    unsigned int valid = list.size();
+    for (const unsigned int i : list) {
+        if (i != 0) {
+            valid--;
+        }
     }
 
-    // Open a file stream in write mode
-    std::string s = "outputDay2.txt";
-    std::ofstream file(s);
 
-    // Check if the file was opened successfully
-    if (!file.is_open()) {
-        std::cerr << "Failed to open the file: " << s << std::endl;
-        return 1;
-    }
-
-    // Write each element of the vector to the file, one per line
-    for (const int& value : list) {
-        file << value << '\n';
-    }
-
-    // Close the file
-    file.close();
-    std::cout << "Vector written to " << s << " successfully!" << std::endl;
-
-    std::cout << all << std::endl;
+    std::cout << "finished" << std::endl;
 }
